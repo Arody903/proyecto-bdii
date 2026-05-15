5a20c97e3da65c02d213b9e6649cf09c3f3651b0
UMGSamuel <jpatzang3@miumg.edu.gt>
2026-05-14 21:28:08 -0600

    Update ddl_schema.sql

--- sql/ddl_schema.sql
+++ sql/ddl_schema.sql
@@ -90,22 +90,24 @@ END $$;
 CREATE TABLE fact_quejas_default PARTITION OF Fact_Quejas DEFAULT;
 
 
---CREACION DE INDICES
+--CREACION DE INDICES, OPTIMIZAR OLAP
 
---indices para la optimizacion de consultas olap
---indice para filtros temporales y partition pruning
+-- indice para filtros temporales y partition pruning
 CREATE INDEX idx_fact_fecha_creacion
 ON Fact_Quejas(id_fecha_creacion);
 
--- Índice para consultas por tipo de queja
+-- indice para analisis de tipos de queja
 CREATE INDEX idx_fact_tipo_queja
 ON Fact_Quejas(id_tipo_queja);
 
---indice compuesto para filtros por distro y fecha
---es importante porque es compuesto y se usa mucho en dashboards
+-- indice compuesto para consultas por distrito y fecha
 CREATE INDEX idx_fact_distrito_fecha
 ON Fact_Quejas(id_distrito, id_fecha_creacion);
 
--- Índice para análisis por agencia
+-- indice para analisis de desempeño por agencia
 CREATE INDEX idx_fact_agencia
 ON Fact_Quejas(id_agencia);
+
+-- indice para consultas de cierres el mismo dia
+CREATE INDEX idx_fact_cierre_mismo_dia
+ON Fact_Quejas(cerrado_mismo_dia);
